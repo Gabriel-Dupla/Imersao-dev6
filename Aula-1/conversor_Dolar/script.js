b2630ef3abecb3e0499c169a2acc4cecd6617a25
Gabriel Archieve <114693768+Gabriel-Dupla@users.noreply.github.com>
2023-02-06 13:43:15 -0300

    Delete Aula-1 directory

--- Aula-1/conversor_Dolar/script.js
+++
@@ -1,6 +0,0 @@
-var valorEmDolar = 30;
-var cotacaoDoDolar = 5.32;
-var valorEmReal = valorEmDolar * cotacaoDoDolar;
-
-valorEmReal = valorEmReal.toFixed([2]);
-alert("Albert, o valor convertido é R$" + valorEmReal);
